6f5f94b43629ed0bd53eda022e5cf58e5ca5db74
Leonardo Curdi <125520529+LeoCurdi@users.noreply.github.com>
2024-04-01 17:16:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,8 @@
     then we've extracted the URL of the photo from the JSON data returned by the API. 
     Finally, we've updated the src attribute of the img element to display the photo.
 */
-/* 
-    This code will make a request to the NASA API every time the page is loaded in a web browser. 
-    The browser sends an HTTP GET request to the API endpoint to retrieve the JSON data, which is then parsed and displayed on the page.
-
-    There is no built-in mechanism in the code to refresh the data periodically or at certain intervals. 
-    If you want to refresh the data automatically, you can use JavaScript's setInterval() function to make periodic requests to the API endpoint 
-    and update the page with new data. 
-    
-    Doing this will still refresh the data every time the page is loaded, but it will also refresh the data at a declared interval in the event someone keeps the page open
-
-    There is a way to code it such that it makes an api request every 24 hours, regardless of whether the page was ever loaded,
-    cache the data, and use the cached data whenever someone loads the page, so that it doesnt need to make a request every time someone loads the page.
-    This method would make the msot sense if I had regular daily traffic to my site and didnt want to make a bunch of api requests every day.
-    It doesnt really make sense for this project though, because I could go months without anyone accessing the site, so i wouldnt need it to keep making api requests during that time
-*/
 const apiKey = 'api_key=vmq7SyshdUvHyl9B2XsUj0C3N1fNSH34c1mvG4fu';
-const apiUrl = `https://api.nasa.gov/planetary/apod?${apiKey}`; // concattonate the api url with my key
+const apiUrl = `https://api.nasa.gov/planetary/apod?${apiKey}`; // concattonate the api url with the key
 
 fetch(apiUrl)
     .then(response => response.json())
